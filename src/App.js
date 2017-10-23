541f3ba8371102e8bd2ed6f23b48b73ebfa45e5a
chrisjxn <usjacksons@gmail.com>
2017-10-23 12:12:04 -0600

    commit changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      anim1: false
+    }
+  }
+
+  anim1Click(){
+    this.setState({
+      anim1: !this.state.anim1
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,6 +28,14 @@ class App extends Component {
             <div>CONTACT</div>
           </div>
         </nav>
+        <div className="slide"></div>
+        <h1>CSS ANIMATIONS</h1>
+        <div className={ this.state.anim1 ? "square animation1" : "square"}></div>
+        <button onClick={ e => this.anim1Click()}>Click me</button>
+        <div className="tire animation2">
+          <div>*</div>
+        </div>
+        <button>Click me</button>
       </div>
     );
   }
